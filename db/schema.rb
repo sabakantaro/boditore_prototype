f0c005173cf4677397d3853e1dce608a4c6a4f6e
sabakantaro <athegl018891@gmail.com>
2021-07-18 12:56:55 +0900

    bundle update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210319033141) do
+ActiveRecord::Schema.define(version: 2021_03_19_033141) do
 
   create_table "entries", force: :cascade do |t|
     t.integer "user_id"
