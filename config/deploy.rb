1c45fda304440728af5f86c598f98b945f685dc3
sabakantaro <sbkryo290112@yahoo.co.jp>
2021-09-05 09:40:43 +0900

    configファイル編集

--- config/deploy.rb
+++ config/deploy.rb
@@ -48,10 +48,10 @@ set :repo_url, 'git@github.com:sabakantaro/boditore_prototype.git'
 set :branch, 'master'
 
 # deploy先のディレクトリ。 
-set :deploy_to, '/var/www/boditore_prototype'
+set :deploy_to, '/var/www/rails/boditore_prototype'
 
 # シンボリックリンクをはるファイル。(※後述)
-set :linked_files, fetch(:linked_files, []).push('config/settings.yml')
+set :linked_files, fetch(:linked_files, []).push('config/master.key')
 
 # シンボリックリンクをはるフォルダ。(※後述)
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
@@ -100,3 +100,4 @@ namespace :deploy do
         end
     end
 end
+
