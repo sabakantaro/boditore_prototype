6e9da235083fe4046d8f040932d252b09d05530f
sabakantaro <sbkryo290112@yahoo.co.jp>
2021-09-23 15:54:33 +0900

    Add for deploy by capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,7 @@ set :branch, 'master'
 set :deploy_to, '/var/www/rails/boditore_prototype'
 
 # シンボリックリンクをはるファイル
-# set :linked_files, fetch(:linked_files, []).push('config/master.key')
+set :linked_files, fetch(:linked_files, []).push('config/master.key')
 
 # シンボリックリンクをはるフォルダ。
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
