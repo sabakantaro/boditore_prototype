c57aa59a7e747b2a7b75ad852bfb67a43575bb0f
sabakantaro <sbkryo290112@yahoo.co.jp>
2021-09-23 17:32:03 +0900

    Add for deploy by capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -64,4 +64,4 @@
 server '3.113.131.50', user: 'sabakantaro',roles: %w{app db web} 
 
 #デプロイするサーバーにsshログインする鍵の情報を記述
-set :ssh_options, keys: '~/.ssh/boditore_key_rsa' 
+set :ssh_options, keys: '~/.ssh/id_rsa.pub' 
