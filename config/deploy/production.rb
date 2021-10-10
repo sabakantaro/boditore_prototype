dc4611262a29455d9bbdf17227403d5193f45ac3
sabakantaro <sbkryo290112@yahoo.co.jp>
2021-10-10 15:23:53 +0900

    view編集

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -63,5 +63,5 @@
 # EC2サーバーのIP、EC2サーバーにログインするユーザー名、サーバーのロールを記述
 server '3.113.131.50', user: 'sabakantaro',roles: %w{app db web} 
 
-#デプロイするサーバーにsshログインする鍵の情報を記述
-set :ssh_options, keys: '~/.ssh/boditore_key_rsa' 
+#デプロイするサーバーにsshログインする鍵の情報を記述※boditore_key_rsaはCircleCI上になかった為id_rsaに変更
+set :ssh_options, keys: '~/.ssh/id_rsa.pub' 
