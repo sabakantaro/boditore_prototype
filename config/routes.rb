5f8d748285a29eced2a9e9c2e58b6a5645f475f4
sabakantaro <sbkryo290112@yahoo.co.jp>
2021-10-29 16:05:45 +0900

    ゲストログイン修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: {
-    registrations: 'users/registrations',
-    sessions: 'users/sessions',
-    passwords: 'users/passwords'
-  }
+  devise_for :users
+
+  # devise_for :users, controllers: {
+  #   registrations: 'users/registrations',
+  #   sessions: 'users/sessions',
+  #   passwords: 'users/passwords'
+  # }
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
