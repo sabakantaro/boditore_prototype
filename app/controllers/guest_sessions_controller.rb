2b9127f64ef2edf9e1696816d444b222f61cd927
sabakantaro <sbkryo290112@yahoo.co.jp>
2021-10-30 14:17:20 +0900

    ゲストログイン修正

--- app/controllers/guest_sessions_controller.rb
+++ app/controllers/guest_sessions_controller.rb
@@ -1,7 +1,7 @@
 class GuestSessionsController < ApplicationController
   def create
     user = User.find_or_create_by(email: "guest@exapmle.com") do |user|
-      user.password = SecureRandom.urlsafe_base64
+      user.password = password
       user.password_confirmation = user.password
       user.name = "ゲストユーザー"
     end
