cc4cb95d730412ad5b841aaba1eee1fba47029b4
sabakantaro <sbkryo290112@yahoo.co.jp>
2021-10-30 15:09:52 +0900

    ゲストログイン修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   get  '/home',    to: 'home#index'
   get  '/signup',  to: 'users#new'
 
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
 
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
