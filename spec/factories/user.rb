224f9c7ed72d1009a2610413c4592171dc4f342c
sabakantaro <sbkryo290112@yahoo.co.jp>
2021-11-30 07:51:03 +0900

    検索機能等の修正

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,6 +2,8 @@ FactoryBot.define do
   factory :user do
     name                  { Faker::Name.name }
     email                 { Faker::Internet.free_email }
+    profile               { 'よろしくお願いします！' }
+    experience            { '5' }
     password              { '00000000' }
     password_confirmation { '00000000' }
   end
