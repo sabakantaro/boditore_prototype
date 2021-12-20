0d272fa0ed67084c35b3a4f7831a270df26670a4
sabakantaro <sbkryo290112@yahoo.co.jp>
2021-12-21 08:48:20 +0900

    投稿数、お気に入り数、フォロワー数のランキング追加 #5

--- app/controllers/ranks_controller.rb
+++ app/controllers/ranks_controller.rb
@@ -1,7 +1,10 @@
 class RanksController < ApplicationController
   def index
-    # 月間ランキング
-    # @month_user_post_ranks = User.where(id: Post.group(:user_id).where(created_at: Time.current.all_month).order('count(user_id) desc').pluck(:user_id))
-    @all_ranks = Post.find(Favorite.group(:post_id).order('count(post_id) desc').limit(3).pluck(:post_id))
+    #月間お気に入り数ランキング
+    @month_user_favorite_ranks = Post.find(Favorite.group(:post_id).where(created_at: Time.current.all_month).order('count(post_id) desc').limit(3).pluck(:post_id))
+    #月間投稿数ランキング
+    @month_user_post_ranks = User.find(Post.group(:user_id).where(created_at: Time.current.all_month).order('count(user_id) desc').limit(3).pluck(:user_id))
+    #月間フォロワー数ランキング
+    @month_user_relationship_ranks = User.find(Relationship.group(:followed_id).where(created_at: Time.current.all_month).order('count(followed_id) desc').limit(3).pluck(:followed_id))
   end
 end
