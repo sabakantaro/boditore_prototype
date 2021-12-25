db8dff299b5713477c00e62b312f1685d3bcfcd7
sabakantaro <sbkryo290112@yahoo.co.jp>
2021-12-25 15:30:21 +0900

    ユーザー関連画面のフロント実装完了 #12

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,7 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :comments
   has_one_attached :image
-  validates_inclusion_of :experience, in: 1..100, message: 'は1~100年で入力してください'
+  validates_inclusion_of :experience, in: 0..100, message: 'は0~100年で入力してください'
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
