a32872a2f603a034b9f9ba9d45695b53820c7e14
sabakantaro <sbkryo290112@yahoo.co.jp>
2022-01-01 17:16:20 +0900

    通知機能テストに通知削除を追加 #20

--- spec/system/notifications_spec.rb
+++ spec/system/notifications_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'Notifications', type: :system do
   before { ActionController::Base.allow_forgery_protection = true }
   after  { ActionController::Base.allow_forgery_protection = false }
 
-  it 'フォローを通知する' do
+  it '通知する' do
     relationship = create(:relationship)
 
     user = relationship.follower
@@ -51,5 +51,53 @@ RSpec.describe 'Notifications', type: :system do
     expect(current_path).to eq notifications_path
 
     expect(page).to have_content user.name
+  end
+
+  it '通知を削除する' do
+    relationship = create(:relationship)
+
+    user = relationship.follower
+
+    other_user = relationship.followed
+    #ログイン
+    visit root_path
+
+    click_link 'ログイン'
+
+    fill_in 'user_email', with: user.email
+
+    fill_in 'user_password', with: user.password
+
+    click_button 'ログイン'
+
+    visit user_path(other_user)
+
+    find('.follow-button-delete').click
+
+    find('.follow-button-register').click
+
+    expect(current_path).to eq user_path(other_user)
+    #ログアウト
+    visit root_path
+
+    click_link 'ログアウト'
+    #other_userでログイン
+    visit root_path
+
+    click_link 'ログイン'
+
+    fill_in 'user_email', with: other_user.email
+
+    fill_in 'user_password', with: other_user.password
+
+    click_button 'ログイン'
+
+    visit notifications_path
+
+    find('.notification-delete').click
+
+    expect(current_path).to eq notifications_path
+
+    expect(page).to have_content 'おしらせはありません'
   end
 end
