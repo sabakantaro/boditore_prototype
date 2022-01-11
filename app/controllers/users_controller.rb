4f98c29823300bdca1cdeb0f9dbc4c12fd371127
sabakantaro <sbkryo290112@yahoo.co.jp>
2022-01-11 14:59:04 +0900

    ユーザーリスト表示のrailsビュー統一完了 #35

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.all
+    @users = User.all.page(params[:page])
     # 月間フォロワー数ランキング
     @month_user_relationship_ranks = User.find(Relationship.group(:followed_id).where(created_at: Time.current.all_month).order('count(followed_id) desc').limit(10).pluck(:followed_id))
   end
