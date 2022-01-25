714324e9ddb2b4901fae2f9418f014aaae93c8b1
sabakantaro <sbkryo290112@yahoo.co.jp>
2022-01-25 12:06:26 +0900

    Rspec修正 #52

--- spec/system/comments_spec.rb
+++ spec/system/comments_spec.rb
@@ -3,51 +3,51 @@
 require 'rails_helper'
 
 RSpec.describe 'Comments', type: :system do
-  # it '新規投稿する' do
-  #   comment = create(:comment)
+  it '新規投稿する' do
+    comment = create(:comment)
 
-  #   post = comment.post
+    post = comment.post
 
-  #   user = comment.user
-  #   # ログイン
-  #   sign_in user
+    user = comment.user
+    # ログイン
+    sign_in user
 
-  #   visit post_path(post)
+    visit post_path(post)
 
-  #   find('.comment-text').set(comment.text)
+    find('.comment-text').set(comment.text)
 
-  #   click_button '送信'
+    click_button '送信'
 
-  #   expect(current_path).to eq post_path(post)
+    expect(current_path).to eq post_path(post)
 
-  #   expect(page).to have_content comment.text
-  # end
+    expect(page).to have_content comment.text
+  end
 
-  # it 'コメントに返信する' do
-  #   comment = create(:comment)
+  it 'コメントに返信する' do
+    comment = create(:comment)
 
-  #   post = comment.post
+    post = comment.post
 
-  #   user = comment.user
-  #   # ログイン
-  #   visit root_path
+    user = comment.user
+    # ログイン
+    visit root_path
 
-  #   click_link 'ログイン'
+    click_link 'ログイン'
 
-  #   fill_in 'user_email', with: user.email
+    fill_in 'user_email', with: user.email
 
-  #   fill_in 'user_password', with: user.password
+    fill_in 'user_password', with: user.password
 
-  #   click_button 'ログイン'
+    click_button 'ログイン'
 
-  #   visit post_path(post)
+    visit post_path(post)
 
-  #   find('.reply-text').set(comment.text)
+    find('.reply-text').set(comment.text)
 
-  #   click_button '送信'
+    click_button '送信'
 
-  #   expect(current_path).to eq post_path(post)
+    expect(current_path).to eq post_path(post)
 
-  #   expect(page).to have_content comment.text
-  # end
+    expect(page).to have_content comment.text
+  end
 end
